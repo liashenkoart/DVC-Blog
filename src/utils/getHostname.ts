933887987c4014cd857865c70d220d24de1e692a
liashenkoart <liashenkoart@gmail.com>
2023-04-14 05:22:39 -0700

    gethostname via URL

--- src/utils/getHostname.ts
+++ src/utils/getHostname.ts
@@ -1,4 +1,11 @@
+export const getHostname = (str: string = '') => {
+  const { hostname } = new URL(str);
+  return hostname;
+};
+
+/*
 export const getHostname = (url: string = '') => {
   const matches = String(url).match(/^(?:https?:)?(?:\/\/)?([^\/\?]+)/i);
   return matches && matches[1];
 };
+*/
